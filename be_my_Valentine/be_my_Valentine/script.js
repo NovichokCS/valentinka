8be906bcdca524a8559b82420ce03cdd53fff95c
NovichokCS <jetsman1@mail.ru>
2026-02-13 16:13:46 +0300

    Add files via upload

---
+++ be_my_Valentine/be_my_Valentine/script.js
@@ -0,0 +1,27 @@
+const messages = [
+    "Ты уверена?",
+    "Точно уверена??",
+    "Ты не сомневаешься?",
+    "Любимка, пожалуйста...",
+    "Просто подумай!",
+    "Если ты скажешь 'нет', мне будет очень грустно...",
+    "Мне будет очень грустно...",
+    "Мне будет очень-очень грустно...",
+    "Ладно, хорошо, я больше не буду спрашивать...",
+    "Шучу, пожалуйста, скажи 'да'! ❤️"
+];
+
+let messageIndex = 0;
+
+function handleNoClick() {
+    const noButton = document.querySelector('.no-button');
+    const yesButton = document.querySelector('.yes-button');
+    noButton.textContent = messages[messageIndex];
+    messageIndex = (messageIndex + 1) % messages.length;
+    const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
+    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+}
+
+function handleYesClick() {
+    window.location.href = "yes_page.html";
+}
