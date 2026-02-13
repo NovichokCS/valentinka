c375b5e898e38d5e3325bebe3252a78fd13f478b
NovichokCS <jetsman1@mail.ru>
2026-02-13 15:50:57 +0300

    script.js

--- be_my_Valentine/script.js
+++ be_my_Valentine/script.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ты уверена?",
+    "Точно уверена??",
+    "Ты не сомневаешься?",
+    "Любимка, пожалуйста...",
+    "Просто подумай!",
+    "Если ты скажешь "нет", мне будет очень грустно...",
+    "Мне будет очень грустно...",
+    "Мне будет очень-очень грустно...",
+    "Ладно, хорошо, я больше не буду спрашивать...",
+    "Шучу, пожалуйста, скажи "да"! ❤️"
 ];
 
 let messageIndex = 0;
